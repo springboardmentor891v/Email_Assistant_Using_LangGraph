25701fa5700fb1ef07d9d15095f98eae1a6ff9ca
Harshitha2347 <harshithababu2316@gmail.com>
2026-02-05 22:01:48 +0530

    Delete evaluation.py

--- evaluation.py
+++
@@ -1,9 +0,0 @@
-import os
-from langsmith import traceable
-
-def run_with_tracing(agent, state):
-    @traceable(name="Email Agent Run")
-    def _run():
-        return agent.invoke(state)
-
-    return _run()
