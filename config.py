e137f8e7a49b4a060e1246a795ee0001e5bd7c61
Harshitha2347 <harshithababu2316@gmail.com>
2026-02-05 22:01:07 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
-LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT", "email-agent")
