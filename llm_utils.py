9cb9fa3e56788e1f5bfaba4016ea0e090393cf5e
Harshitha2347 <harshithababu2316@gmail.com>
2026-02-05 22:02:27 +0530

    Delete llm_utils.py

--- llm_utils.py
+++
@@ -1,8 +0,0 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from config import OPENAI_API_KEY
-
-llm = ChatGoogleGenerativeAI(
-    model="gemma-3-27b-it",
-    google_api_key=OPENAI_API_KEY,
-    temperature=0.3
-)
